824445034f6b6e477e6a1c7436ae02915af6a725
Pascal Hertleif <killercup@gmail.com>
2012-05-27 13:11:18 +0200

    Enhance JS to not give a shit if unnecessary

--- javascripts/theme.js
+++ javascripts/theme.js
@@ -7,7 +7,9 @@ document.write('<meta name="viewport" content="width=device-width, initial-scale
 !function(a,b){typeof module!="undefined"?module.exports=b():typeof define=="function"&&typeof define.amd=="object"?define(b):this[a]=b()}("domready",function(a){function m(a){l=1;while(a=b.shift())a()}var b=[],c,d=!1,e=document,f=e.documentElement,g=f.doScroll,h="DOMContentLoaded",i="addEventListener",j="onreadystatechange",k="readyState",l=/^loade|c/.test(e[k]);return e[i]&&e[i](h,c=function(){e.removeEventListener(h,c,d),m()},d),g&&e.attachEvent(j,c=function(){/^c/.test(e[k])&&(e.detachEvent(j,c),m())}),a=g?function(c){self!=top?l?c():b.push(c):function(){try{f.doScroll("left")}catch(b){return setTimeout(function(){a(c)},50)}c()}()}:function(a){l?a():b.push(a)}});
 
 function move_into(container, thing) {
-  document.getElementById(container).appendChild(document.getElementById(thing));
+  try {
+    document.getElementById(container).appendChild(document.getElementById(thing));
+  } catch(err) {};
 }
 
 function insertAfter( referenceNode, newNode ) {
@@ -28,6 +30,7 @@ domready(function () {
   // top menu toggle button
   (function(){
     var title = document.getElementById("header").getElementsByTagName("h1")[0];
+    if (!title) return;
     var button = document.createElement('a');
     button.innerHTML = "nav";
     button.setAttribute("class", "toggle-button for-topmenu");
@@ -49,6 +52,7 @@ domready(function () {
   // nav toggle button
   (function(){
     var menu = document.getElementById("main-menu");
+    if (!menu) return;
     var button = document.createElement('a');
     button.innerHTML = "nav";
     button.setAttribute("class", "toggle-button for-main-menu");
